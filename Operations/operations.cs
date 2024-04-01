27ee32078d74ad49666333bdc26b9b8c99dae4a7
BlissfulBonsai <tiagozfiori@gmail.com>
2024-04-01 19:17:26 -0300

    First commit. Calculator is able to repeat the add operation for now (just need to implement the repeat function on the other operations). Also, the calculator is able to use the last result as one of the values for the operation.

---
+++ Operations/operations.cs
@@ -0,0 +1,11 @@
+﻿namespace Operations
+{
+    public class OperationClass
+    {
+        float addNumbers(float numberOne, float numberTwo)
+        {
+            float result = numberOne + numberTwo;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿float finalResult = 0;
+
+bool useResult()
+{
+    if (finalResult != 0)
+    {
+        Console.WriteLine("Would you like to use the value from the last operation as a value for the operation? 'yes' - Yes");
+        string userChoice = Console.ReadLine();
+        if (userChoice == "yes")
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+float addNumbers(float numberOne, float numberTwo)
+{
+    if (useResult())
+    {
+        float result = finalResult + numberTwo;
+        finalResult = result;
+        return result;
+    }
+    else
+    {
+        float result = numberOne + numberTwo;
+        finalResult = result;
+        return result;
+    }
+    
+}
+
+float subtractNumbers(float numberOne, float numberTwo)
+{
+    float result = numberOne + numberTwo;
+    finalResult = result;
+    return result;
+}
+
+float multiplyNumbers(float numberOne, float numberTwo)
+{
+    float result = numberOne + numberTwo;
+    finalResult = result;
+    return result;
+}
+
+float divideNumbers(float numberOne, float numberTwo)
+{
+    float result = numberOne + numberTwo;
+    finalResult = result;
+    return result;
+}
+
+bool sameOperation()
+{
+    Console.WriteLine("Would you like to perform the same operation?");
+    string userChoice = Console.ReadLine();
+    if(userChoice == "yes")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+
+string userChoice = "";
+
+while(userChoice != "exit")
+{
+    Console.WriteLine("Specify an operation:");
+    Console.WriteLine("'exit' - Exit\n'add' - Add\n'subtract' - Subtract\n'multiply' - Multiply\n'divide' - Divide\n");
+    userChoice = Console.ReadLine();
+
+    switch(userChoice)
+    {
+        case "add":
+
+            do
+            {
+                Console.WriteLine("Insert the values for the numbers, insert a value, press enter, then insert another value and press enter");
+                float firstNumber = float.Parse(Console.ReadLine());
+                float secondNumber = float.Parse(Console.ReadLine());
+                addNumbers(firstNumber, secondNumber);
+                Console.WriteLine("The result is: " + finalResult + "\n");
+            } while (sameOperation());
+            
+            break;
+            
+    }
+}
+
+
+
+
